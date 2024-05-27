71e631047f66a2f8ad45a93824b5ef9663cd0968
thorafinn <singh.roshan.1444@gmail.com>
2024-05-27 23:07:25 +0530

    BugFix - CorsUrl bugfix

--- src/main/java/com/streamit/security/StreamItUIInterceptorAppConfig.java
+++ src/main/java/com/streamit/security/StreamItUIInterceptorAppConfig.java
@@ -26,6 +26,6 @@ public  class StreamItUIInterceptorAppConfig implements WebMvcConfigurer{
 	
 	 @Override
 	 public void addCorsMappings(CorsRegistry registry) {
-	        registry.addMapping("/**");
+	        registry.addMapping("/*").allowedOrigins("*");
 	 }
 }
